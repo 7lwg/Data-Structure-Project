a5f3aeb94ae142f9d6c6a78a4f472f841e27b8e7
7lwg <ahmedothmanalhalwagy@gmail.com>
2026-04-05 00:11:24 +0200

    Connecting Phase 5 to the main application menu

--- src/main.py
+++ src/main.py
@@ -3,17 +3,18 @@ from phase1_indexer import run_phase1
 from phase2_tracker import run_phase2
 from phase3_parser import run_phase3
 from phase4_buffer import run_phase4
+from phase5_trees import run_phase5
 # ── Main menu ──────────────────────────────────────────────────
 def main():
     while True:
         print("\n==================================================")
         print("   Welcome to DataFlow Pro - NileMart ETL Engine  ")
         print("==================================================")
-        print("1. Run Phase 1: Query Optimizer (Sorting & Searching)")
-        print("2. Run Phase 2: Applied Steps Tracker (Linked Lists)")  
+        print("1. Query Optimizer (Sorting & Searching)")
+        print("2. Applied Steps Tracker (Linked Lists)")  
         print("3. Evaluate DAX Formula (Stack)")
-        # print("4. Ingest Live Data (Queue)")
-        # print("5. Analyze Org Chart Sales (Trees)")
+        print("4. Ingest Live Data (Queue)")
+        print("5. Analyze Org Chart Sales (Trees)")
         print("6. Exit")
         choice = input("\nSelect an option: ")
         if choice == "1":
@@ -24,6 +25,8 @@ def main():
             run_phase3()
         elif choice == "4":                 
             run_phase4()
+        elif choice == "5":                 
+            run_phase5()
         elif choice == "6":
             print("Shutting down DataFlow Pro. Masalama!")
             sys.exit()
